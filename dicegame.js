d40a900684285f0543c9060c09b33ef092e93563
Stella Ma <sm873x@gmail.com>
2016-07-05 15:45:00 -0400

    Added rollDice function to game object

--- dicegame.js
+++ dicegame.js
@@ -1,7 +1,28 @@
 var diceGame = {
-    startDateTime: new Date(),
+    timeStamp: new Date(),
     rounds: [],
-    diceRoll: function diceRoll() {
+    rollDice: function rollDice() {
+        var dice1 = Math.ceil(Math.random() * 6);
+        document.getElementById('dice1').innerHTML = dice1;
+
+        var dice2 = Math.ceil(Math.random() * 6);
+        document.getElementById('dice2').innerHTML = dice2;
+
+        if ( (dice1 + dice2) === 7 || (dice1 + dice2) === 11 ) {
+            document.getElementById('gameStatus').innerHTML = 'Winner!';
+        } else {
+            document.getElementById('gameStatus').innerHTML = 'Try Again!';
+        }
 
     }
 };
+
+diceGame.rollDice();
+
+console.log( diceGame.timeStamp );
+
+
+
+
+//TIME STAMP
+document.getElementById('gameTimeStamp').innerHTML = diceGame.timeStamp;
